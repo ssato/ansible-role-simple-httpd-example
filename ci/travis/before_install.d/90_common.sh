79f660ccfe7dda795d299eb269d645f7f0e76185
Satoru SATOH <satoru.satoh@gmail.com>
2020-08-19 00:00:29 +0900

    fix: add workarounds to install yamllint and ansible-lint using apt-get to avoid command not found error due to unknown reasosns

--- ci/travis/before_install.d/90_common.sh
+++ ci/travis/before_install.d/90_common.sh
@@ -2,6 +2,6 @@
 set -ex
 
 sudo apt-get update -qq
-sudo apt-get install -y bash iproute2 iputils-ping
+sudo apt-get install -y bash bats iproute2 iputils-ping ansible-lint yamllint
 
 # vim:sw=2:ts=2:et:
